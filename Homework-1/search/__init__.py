870c8b60b069768c1e4647d11e37ebb55c76c124
aashishyadavally <aashish.yadavally1995@gmail.com>
2021-02-28 12:43:17 -0700

    Add file with consolidated module imports

--- Homework-1/search/__init__.py
+++ Homework-1/search/__init__.py
@@ -1,4 +1,17 @@
+"""
+Implementations for uninformed and informed search strategies:
+(a) Depth-First Search : ``search.depth_first.DFS``
+(b) Breadth-First Search : ``search.breadth_first.BFS``
+(c) Iterative-Deepening Search : ``search.iterative_deepening.IDS``
+(d) A-Star Search: ``search.aster.AStar``
+
+Each requires multiple membership-tests in the search algorithm.
+Membership-test in a list has a time complexity of O(n), while that
+in a set is O(1). Thus, sets are used for this purpose.
+"""
+
 from search.utils import *
 from search.depth_first import DFS
+from search.iterative_deepening import IDS
 from search.breadth_first import BFS
 from search.astar import AStar
