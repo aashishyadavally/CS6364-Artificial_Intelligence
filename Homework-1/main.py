fe7e06e858c0a18b0871fcc3fca9ff69bed300bf
aashishyadavally <aashish.yadavally1995@gmail.com>
2021-02-28 12:42:00 -0700

    Add entry point into ``search`` module

--- Homework-1/main.py
+++ Homework-1/main.py
@@ -1,3 +1,10 @@
+"""Main script for running the search algorithm implementations
+in ``search`` module.
+
+To get the options, run:
+    $ python main.py --help 
+"""
+
 import argparse
 from search import *
 
@@ -9,21 +16,29 @@ if __name__ == '__main__':
                         (a) \'*\' refers to the blank tile in 8-Puzzle Problem. \
                         (b) First line -> Input State, Second line -> Goal State.')
     parser.add_argument('--algorithm', dest='algo', type=str,
-                        choices=['dfs', 'bfs', 'astar1', 'astar2'],
+                        choices=['dfs', 'ids', 'astar1', 'astar2'],
                         required=True, help='Search algorithm to run, choices are: \
-                        (a) dfs: Depth-First Search (b) bfs: Breadth-First Search \
+                        (a) dfs: Depth-First Search \
+                        (b) ids: Iterative-Deepening Search \
                         (c) astar1: A* algorithm with heuristic 1 \
-                        (d) astar2: A* algorithm with heuristic 2')
+                            (Number of misplaced tiles) \
+                        (d) astar2: A* algorithm with heuristic 2 \
+                            (Manhattan Distance)')
+    parser.add_argument('--depth-limit', dest='depth_limit', type=int,
+                        default=10, help='Depth limit for search traversal. \
+                        Valid for Depth-First and Iterative-Deepening Search algorithms.')
 
     args = parser.parse_args()
 
-    input_state, goal_state = read_states(args.input_path)
+    start_state, goal_state = read_states(args.input_path)
 
     if args.algo == 'dfs':
-        print('BFS')
-    elif args.algo == 'bfs':
-        print('DFS')
+        DFS(start_state, goal_state, args.depth_limit)
+    elif args.algo == 'ids':
+        IDS(start_state, goal_state, args.depth_limit)
     elif args.algo == 'astar1':
-        print('AStar1')
+        AStar(start_state, goal_state, args.depth_limit,
+              heuristic='misplaced')
     elif args.algo == 'astar2':
-        print('AStar2')
+        AStar(start_state, goal_state, args.depth_limit,
+              heuristic='manhattan')
